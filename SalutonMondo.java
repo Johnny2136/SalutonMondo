565e1dec65e179219f4b1a658a91cdb1de0bffbf
Johnny <johnny@localhost.localdomain>
2016-08-08 13:19:51 -0400

    Untracked commit to local branch

--- SalutonMondo.java
+++ SalutonMondo.java
@@ -1,6 +1,7 @@
-public class SalutonMonde {
+class SalutonMonde {
    public static void main(String[] args) {
-       String greeting = "Saluton Mondo, I'm a Master branch";
+       String greeting = "Saluton Mondo, I'm a Master branch" endl;
+       String greeting = " This is Espranto for Hello world!";
        System.out.println(greeting);
    }
 }
