10dc67c1a971118a1e81f48366ea8879e9401628
Johnny <johnny@localhost.localdomain>
2016-08-08 07:47:50 -0400

    This is commit #1

---
+++ SalutonMondo.java
@@ -0,0 +1,7 @@
+public class SalutonMonde {
+   public static void main(String[] args) {
+       String greeting = "Saluton Mondo, I'm a Master branch";
+       System.out.println(greeting);
+   }
+}
+
