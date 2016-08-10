aebe6486da923601fc3edcf9abe4cbb466236668
Johnny <johnny@localhost.localdomain>
2016-08-10 14:56:02 -0400

    Change1

--- SalutonMondo.java
+++ SalutonMondo.java
@@ -1,11 +1,10 @@
 class SalutonMonde {
    public static void main(String[] args) {
-<<<<<<< HEAD
+
        String greeting = "Saluton Mondo, I'm a Master branch" endl;
        String greeting = " This is Espranto for Hello world!";
-=======
-       String greeting = "Saluton Mondo, I'm a Development branch";
->>>>>>> 05d4554bb05650b6041329b932d27e7d331f7dca
+
+       String greeting = "Saluton Mondo, I'm a new change branch";
        System.out.println(greeting);
    }
 }
