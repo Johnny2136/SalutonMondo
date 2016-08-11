5b1b00f21f01607ef5fa842a8b233bffa9fb86c3
Johnny <johnny@localhost.localdomain>
2016-08-11 06:50:09 -0400

    Commit from fedora box

--- SalutonMondo.java
+++ SalutonMondo.java
@@ -1,6 +1,6 @@
 class SalutonMonde {
    public static void main(String[] args) {
-
+	int t =0;
        String greeting = "Saluton Mondo, I'm a Master branch" endl;
        String greeting = " This is Espranto for Hello world!";
 
